200997072ed7fdbfaacc1ce711339ff085b55efa
brunomoreira <brunnomoreira.silva@gmail.com>
2015-03-23 09:03:32 -0300

    changes

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ public class Main
 	public static void main ()
 	{
 		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
 	}
 }
