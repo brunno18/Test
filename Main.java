6220ec6b731d0064740ef054e7a263f174badc9e
brunomoreira <brunnomoreira.silva@gmail.com>
2015-04-20 08:42:39 -0300

    teste

--- Main.java
+++ Main.java
@@ -3,5 +3,17 @@ public class Main
 	public static void main ()
 	{
 		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
+		System.out.println( "Sorry" );
 	}
 }
